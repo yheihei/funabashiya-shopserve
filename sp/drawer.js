247451fdb510ef4895ae617c24f77a5ea857a6fd
Yohei Kokubo <yhei.register@gmail.com>
2020-08-26 19:50:09 +0900

    レビュー指摘修正 SPにドロワーメニューを入れる #1

--- sp/drawer.js
+++ sp/drawer.js
@@ -1,16 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const drawer = document.querySelector('.drawer');
-  if (!drawer) {
-    return;
-  }
-  
-  document.querySelectorAll('.js-drawer-button').forEach(function(drawerButton) {
-    drawerButton.addEventListener('click', function() {
-      if (drawer.classList.contains('drawer--active')) {
-        drawer.classList.remove('drawer--active');
-      } else {
-        drawer.classList.add('drawer--active');
-      }
+  /**
+   * 初期化処理
+   *
+   * @return {void}
+   */
+  function init() {
+    // ドロワー要素を取得
+    const drawer = document.querySelector('.drawer');
+    if (!drawer) {
+      // ドロワーがない場合は処理しない
+      return;
+    }
+    // ドロワーの開閉ボタンの初期化
+    document.querySelectorAll('.js-drawer-button').forEach(function(drawerButton) {
+      drawerButton.addEventListener('click', drawerButtonClickEvent);
     });
-  });
+  }
+
+  /**
+   * ドロワーの開閉ボタンがクリックされた時の処理
+   *
+   * @return {void}
+   */
+  function drawerButtonClickEvent() {
+    toggleDrawer(drawer);
+  }
+
+  /**
+   * 指定の要素のドロワーを開閉する
+   *
+   * @param {Element} drawerElement
+   * @return {void}
+   */
+  function toggleDrawer(drawerElement) {
+    if (drawerElement.classList.contains('drawer--active')) {
+      drawerElement.classList.remove('drawer--active');
+    } else {
+      drawerElement.classList.add('drawer--active');
+    }
+  }
+
+  init();
 });
